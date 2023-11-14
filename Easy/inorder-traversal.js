c22a3c0f18a0a6cdac28b4eacc99b53cf768c512
Anurag Arwalkar <anurag.arwalkar@globant.com>
2023-11-14 14:51:38 +0530

    inorder traversal

---
+++ Easy/inorder-traversal.js
@@ -0,0 +1,44 @@
+function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+}
+
+const treeNode = new TreeNode(1, new TreeNode(4, new TreeNode(6), new TreeNode(7)), new TreeNode(2, new TreeNode(3), new TreeNode(5)))
+
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+// var inorderTraversal = function (root, nodes = []) {
+//     if(!root) {
+//         return [];
+//     }
+
+//     inorderTraversal(root.left, nodes);
+//     nodes.push(root.val);
+//     inorderTraversal(root.right, nodes);
+
+
+//     return nodes;
+// };
+
+var inorderTraversal = function (root) {
+    let current = root;
+    const result = [];
+    const stack = [];
+
+    while(current || stack.length) {
+        while(current) {
+            stack.push(current);
+            current = current.left;
+        }
+        current = stack.pop();
+        result.push(current.val);
+        current = current.right;
+    }
+
+    return result;
+};
+
+console.log(inorderTraversal(treeNode))
