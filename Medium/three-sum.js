68999d15e29e7678fe187c1c65c1447ddff921ac
Anurag Arwalkar <anurag.arwalkar@globant.com>
2023-11-28 23:27:54 +0530

    update three and two sum

--- Medium/three-sum.js
+++ Medium/three-sum.js
@@ -25,9 +25,49 @@ var threeSum = function (nums) {
     return result;
 };
 
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function (nums) {
+    const result = [];
+
+    nums.sort((a, b) => a - b);
+
+   for(let i = 0; i < nums.length; i++) {
+
+    if(i > 0 && nums[i] === nums[i-1]) {
+        continue;
+    }
+
+    let start = i + 1;
+    let end = nums.length - 1;
+
+    while(start < end) {
+        const total = nums[i] + nums[start] + nums[end];
+
+        if(total > 0) {
+            end--;
+        }else if(total < 0) {
+            start++;
+        }else{
+            result.push([nums[i], nums[start], nums[end]]);
+            end--;
+            while(nums[end] === nums[end - 1] && start < end) {
+                end--;
+            }
+        }
+    }
+     
+   }
+
+    return result;
+};
+
 
 
 console.log(threeSum([-1,0,1,2,-1,-4]))
+console.log(threeSum([0, 0, 0, 0]));
 // console.log(threeSum([0, 0, 0, 0]))
 // console.log([2,7,11,15]);
 // console.log(twoSum([2, 3, 4], 6));
--- Medium/two-sum-2.js
+++ Medium/two-sum-2.js
@@ -16,3 +16,29 @@ var twoSum = function (numbers, target) {
         numbersIndexMap[numbers[i]] = i;
     };
 };
+
+
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (numbers, target) {
+    let start = 0;
+    let end =  numbers.length - 1;
+    
+    while(start < end) {
+        if((numbers[start] + numbers[end]) === target) {
+            return [start + 1,  end + 1]
+        }
+
+        if((numbers[start] + numbers[end]) > target) {
+            end--;
+        }else {
+            start++;
+        }
+    }
+};
+
+
+console.log(twoSum([2,7,11,15],9))
