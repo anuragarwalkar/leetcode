c5f6693978de54c0bf93a35dbb2818aed8cb0d0a
Anurag Arwalkar <anurag.arwalkar@globant.com>
2023-12-29 18:44:56 +0530

    valid binary search tree

---
+++ Medium/valid-binary-search-tree.js
@@ -0,0 +1,61 @@
+function TreeNode(val) {
+    this.val = val;
+    this.left = this.right = null;
+}
+
+function convertArrayToTreeNode(inputArray) {
+    let nodes = {};
+    
+    for(let i = 0; i < inputArray.length; i++) {
+        const item = inputArray[i];
+        nodes[i] = item ? new TreeNode(item) : null;
+    }
+
+    for(let i = 0; i < inputArray.length; i++) {
+      const leftChildIndex = 2 * i + 1;
+      const rightChildIndex = 2 * i + 2;
+
+      if(nodes[i]) {
+          nodes[i].left = nodes[leftChildIndex]
+          nodes[i].right = nodes[rightChildIndex]
+      }
+    }
+
+    return nodes[0]
+}
+
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function(root) {
+    
+    function isValid(node, left, right) {
+        if(!node) {
+            return true;
+        }
+
+        if(!(node.val < right && node.val > left)) {
+            return false; 
+        }
+
+        return isValid(node.left, left, node.val) && isValid(node.right, node.val, right)
+    }
+
+    return isValid(root, Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER)
+};
+
+
+console.log(isValidBST(convertArrayToTreeNode([5,4,6,null,null,3,7])))
