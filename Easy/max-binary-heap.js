31da66921b926c0992b8ae1f4d6858b7ffe5be25
Anurag Arwalkar <anurag.arwalkar@globant.com>
2024-01-23 16:58:38 +0530

    update

--- Easy/max-binary-heap.js
+++ Easy/max-binary-heap.js
@@ -1,6 +1,6 @@
 class MaxBinaryHeap {
     constructor() {
-        this.values = [];
+       this.values = []; 
     }
 
     enqueue(value) {
@@ -8,58 +8,80 @@ class MaxBinaryHeap {
         this._bubbleUp();
     }
 
+
     _bubbleUp() {
         let index = this.values.length - 1;
-        let element = this.values[index];
-
+        let element = this.values[index];   
+        
         while(index > 0) {
-            const parentIndex = Math.floor((index - 1) / 2) 
-            const parent = this.values[parentIndex];
+            let parentIndex = Math.floor((index - 1) / 2);
+            let parent = this.values[parentIndex];
 
             if(element <= parent) break;
 
             this.values[parentIndex] = element;
             this.values[index] = parent;
+
             index = parentIndex;
         }
     }
 
     dequeue() {
         const max = this.values[0];
-        const last = this.values.pop();
-        this.values[0] = last;
+        const lastValue = this.values.pop();
+
+        if(this.values.length === 0) {
+            return max;
+        }
+
+        this.values[0] = lastValue;
         this._bubbleDown();
-        return max;
+        return max
     }
 
     _bubbleDown() {
         let index = 0;
-
+        let value = this.values[index];
+        
         while(index < this.values.length) {
-            let leftChildIndex = Math.floor(2 * index + 1);
-            let rightChildIndex = Math.floor(2 * index + 2);
+            let leftChildIndex = Math.floor(index * 2) + 1;
+            let righChildIndex = Math.floor(index * 2) + 2;
+            
             let leftChild = this.values[leftChildIndex];
-            let rightChild = this.values[rightChildIndex];
+            let rightChild = this.values[righChildIndex];
             
-            let swap = false;
-            
-            if(leftChild > this.values[index]) {
-                this.values[leftChildIndex] = this.values[index];
+            if(value < leftChild && leftChild > rightChild) {
                 this.values[index] = leftChild;
+                this.values[leftChildIndex] = value;
                 index = leftChildIndex;
-                swap = true;
-            }else if(rightChild > this.values[index]) {
-                this.values[rightChildIndex] = this.values[index];
+            }else if(value < rightChild && rightChild > leftChild) {
                 this.values[index] = rightChild;
-                index = rightChildIndex;
-                swap = true;
-            }
-
-            if(!swap) {
+                this.values[righChildIndex] = value;
+                index = righChildIndex;
+            }else {
                 break;
             }
-            
-        }
+}               
     }
-
 }
+
+
+const instance = new MaxBinaryHeap();
+
+instance.enqueue(10);
+instance.enqueue(100);
+instance.enqueue(200);
+instance.enqueue(300);
+instance.enqueue(500);
+instance.enqueue(1000);
+
+// instance.dequeue();
+// instance.dequeue();
+// instance.dequeue();
+// instance.dequeue();
+// instance.dequeue();
+// instance.dequeue();
+
+console.log('instance:', instance);
+
+
